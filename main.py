9adfb2aa593bd875da6c8bf6bc7a5ebfa2746444
Vladyslav Lopatin <ilopatin3@gmail.com>
2025-01-17 00:20:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
-from tempfile import mkdtemp
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from tempfile import mkdtemp
 
+# from selenium import webdriver
+# from tempfile import mkdtemp
+# from selenium.webdriver.common.by import By
 
 def handler(event=None, context=None):
     try:
